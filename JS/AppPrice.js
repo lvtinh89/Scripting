eaf4e74bd9aaab89ae9b45edfb87a5ee5fe91df3
Lê Văn Tính <lvtinh1051989@users.noreply.github.com>
2020-03-24 06:34:54 +0700

    Update

---
+++ JS/AppPrice.js
@@ -0,0 +1,152 @@
+//Smart Qx&Surge 
+let isQuantumultX = $task !== undefined;
+let isSurge = $httpClient !== undefined;
+var $task = isQuantumultX ? $task : {};
+var $httpClient = isSurge ? $httpClient : {};
+var $prefs = isQuantumultX ? $prefs : {};
+var $persistentStore = isSurge ? $persistentStore : {};
+var $notify = isQuantumultX ? $notify : {};
+var $notification = isSurge ? $notification : {};
+if (isQuantumultX) {
+    var errorInfo = {
+        error: ''
+    };
+    $httpClient = {
+        get: (url, cb) => {
+            var urlObj;
+            if (typeof (url) == 'string') {
+                urlObj = {
+                    url: url
+                }
+            } else {
+                urlObj = url;
+            }
+            $task.fetch(urlObj).then(response => {
+                cb(undefined, response, response.body)
+            }, reason => {
+                errorInfo.error = reason.error;
+                cb(errorInfo, response, '')
+            })
+        },
+        post: (url, cb) => {
+            var urlObj;
+            if (typeof (url) == 'string') {
+                urlObj = {
+                    url: url
+                }
+            } else {
+                urlObj = url;
+            }
+            url.method = 'POST';
+            $task.fetch(urlObj).then(response => {
+                cb(undefined, response, response.body)
+            }, reason => {
+                errorInfo.error = reason.error;
+                cb(errorInfo, response, '')
+            })
+        }
+    }
+}
+if (isSurge) {
+    $task = {
+        fetch: url => {
+            return new Promise((resolve, reject) => {
+                if (url.method == 'POST') {
+                    $httpClient.post(url, (error, response, data) => {
+                        response.body = data;
+                        resolve(response, {
+                            error: error
+                        });
+                    })
+                } else {
+                    $httpClient.get(url, (error, response, data) => {
+                        response.body = data;
+                        resolve(response, {
+                            error: error
+                        });
+                    })
+                }
+            })
+
+        }
+    }
+}
+if (isQuantumultX) {
+    $persistentStore = {
+        read: key => {
+            return $prefs.valueForKey(key);
+        },
+        write: (val, key) => {
+            return $prefs.setValueForKey(val, key);
+        }
+    }
+}
+if (isSurge) {
+    $prefs = {
+        valueForKey: key => {
+            return $persistentStore.read(key);
+        },
+        setValueForKey: (val, key) => {
+            return $persistentStore.write(val, key);
+        }
+    }
+}
+if (isQuantumultX) {
+    $notification = {
+        post: (title, subTitle, detail) => {
+            $notify(title, subTitle, detail);
+        }
+    }
+}
+if (isSurge) {
+    $notify = function (title, subTitle, detail) {
+        $notification.post(title, subTitle, detail);
+    }
+}
+//end
+
+/*using surge cron*/
+
+const region = "vn"
+const appIds = ["1357536697","1319618742","318906310","834734525","975973861","916366645","777310222","1087099916","1436050646","1444636541","773481107","504001448","920133658","596684220","472591011","708196645","728293409","474764934","1072737282","1112850631","471784765","1300697619","903019028","586166344","1117941080","382418196","1062022008","1274090551","866786833","924695435","1116905928","1018301773","606310581","436577167","503812755","730712409","950519698","1474856599","1422605014","1373567447","1357644265","1210562295","1282297037","932747118","1252015438","1443988620","691121579","558818638","306257910","492648096","447444215","1117998129","364502063","363317633","669835030","295798315","358207332","688831692","333710667","317107309","1422480068","749132901","499470113","360593530","1444383602","425073498","469343097","1118115766","556666026","527358348","1187265767","1389752090","479516143"]
+
+var cacheData = $persistentStore.read()
+if (!cacheData) {
+    cacheData = {}
+} else {
+    cacheData = JSON.parse(cacheData)
+}
+
+$httpClient.post('https://itunes.apple.com/lookup?id=' + appIds + "&country=" + region, function (error, response, data) {
+    if (error) {
+        console.log(error);
+        $notification.post("App Pricer", "bad connection")
+        $done()
+    } else {
+        let appData = JSON.parse(data).results
+        let priceChanged = ""
+        let newAppAdded = ""
+        for (var i = 0; i < appData.length; i++) {
+            if (cacheData[appData[i].trackId]) {
+                if (appData[i].formattedPrice != cacheData[appData[i].trackId].price) {
+                    priceChanged = priceChanged + "🏷 " + appData[i].trackName + "  " + cacheData[appData[i].trackId].price + " → " + appData[i].formattedPrice + "\n"
+                    cacheData[appData[i].trackId].price = appData[i].formattedPrice
+                }
+            } else {
+                newAppAdded = newAppAdded + "🏷 " + appData[i].trackName + "  " + appData[i].formattedPrice + "\n"
+                cacheData[appData[i].trackId] = {
+                    name: appData[i].trackName,
+                    price: appData[i].formattedPrice
+                }
+            }
+        }
+        if (priceChanged) {
+            $notification.post("Price Changed", "", priceChanged)
+        }
+        if (newAppAdded) {
+            $notification.post("New Apps Added", "", newAppAdded)
+        }
+        $persistentStore.write(JSON.stringify(cacheData))
+        $done()
+    }
+})
