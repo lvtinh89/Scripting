c16648ae6973942172be170f23d6bf7bf99cc70f
Lê Văn Tính <59911146+lvtinh1051989@users.noreply.github.com>
2020-03-25 12:27:52 +0700

    Add files via upload

---
+++ JS/AppMonitor.js
@@ -0,0 +1,216 @@
+//Smart Qx&Surge + @Neurogam
+let isQuantumultX = $task !== undefined;
+let isSurge = $httpClient !== undefined;
+var $task = isQuantumultX ? $task : {};
+var $httpClient = isSurge ? $httpClient : {};
+var $prefs = isQuantumultX ? $prefs : {};
+var $persistentStore = isSurge ? $persistentStore : {};
+var $notify = isQuantumultX ? $notify : {};
+var $notification = isSurge ? $notification : {};
+if (isQuantumultX) {
+    var errorInfo = {
+        error: ''
+    };
+    $httpClient = {
+        get: (url, cb) => {
+            var urlObj;
+            if (typeof (url) == 'string') {
+                urlObj = {
+                    url: url
+                }
+            } else {
+                urlObj = url;
+            }
+            $task.fetch(urlObj).then(response => {
+                cb(undefined, response, response.body)
+            }, reason => {
+                errorInfo.error = reason.error;
+                cb(errorInfo, response, '')
+            })
+        },
+        post: (url, cb) => {
+            var urlObj;
+            if (typeof (url) == 'string') {
+                urlObj = {
+                    url: url
+                }
+            } else {
+                urlObj = url;
+            }
+            url.method = 'POST';
+            $task.fetch(urlObj).then(response => {
+                cb(undefined, response, response.body)
+            }, reason => {
+                errorInfo.error = reason.error;
+                cb(errorInfo, response, '')
+            })
+        }
+    }
+}
+if (isSurge) {
+    $task = {
+        fetch: url => {
+            return new Promise((resolve, reject) => {
+                if (url.method == 'POST') {
+                    $httpClient.post(url, (error, response, data) => {
+                        response.body = data;
+                        resolve(response, {
+                            error: error
+                        });
+                    })
+                } else {
+                    $httpClient.get(url, (error, response, data) => {
+                        response.body = data;
+                        resolve(response, {
+                            error: error
+                        });
+                    })
+                }
+            })
+
+        }
+    }
+}
+if (isQuantumultX) {
+    $persistentStore = {
+        read: key => {
+            return $prefs.valueForKey(key);
+        },
+        write: (val, key) => {
+            return $prefs.setValueForKey(val, key);
+        }
+    }
+}
+if (isSurge) {
+    $prefs = {
+        valueForKey: key => {
+            return $persistentStore.read(key);
+        },
+        setValueForKey: (val, key) => {
+            return $persistentStore.write(val, key);
+        }
+    }
+}
+if (isQuantumultX) {
+    $notification = {
+        post: (title, subTitle, detail) => {
+            $notify(title, subTitle, detail);
+        }
+    }
+}
+if (isSurge) {
+    $notify = function (title, subTitle, detail) {
+        $notification.post(title, subTitle, detail);
+    }
+}
+//end
+
+console.log("🟢 APPPRICE");
+let apps=["1357536697","1319618742","318906310","834734525","975973861","916366645","777310222","1087099916","1436050646","1444636541","773481107","504001448","920133658","596684220","472591011","708196645","728293409","474764934","1072737282","1112850631","471784765","1300697619","903019028","586166344","1117941080","382418196","1062022008","1274090551","866786833","924695435","1116905928","1018301773","606310581","436577167","503812755","730712409","950519698","1474856599","1422605014","1373567447","1357644265","1210562295","1282297037","932747118","1252015438","1443988620","691121579","558818638","306257910","492648096","447444215","1117998129","364502063","363317633","669835030","295798315","358207332","688831692","333710667","317107309","1422480068","749132901","499470113","360593530","1444383602","425073498","469343097","1118115766","556666026","527358348","1187265767","1389752090","479516143"];
+let reg="vn";
+let notifys=[];
+format_apps(apps);
+function format_apps(x) {
+    let apps_f={};
+    x.forEach((n)=>{
+        if(/^[a-zA-Z0-9:/|\-_\s]{1,}$/.test(n))
+        {
+            n=n.replace(/[/|\-_\s]/g,":");
+            let n_n=n.split(":");
+            if(n_n.length===1){
+                if(apps_f.hasOwnProperty(reg)){
+                    apps_f[reg].push(n_n);
+                }
+                else
+                {
+                    apps_f[reg]=[];
+                    apps_f[reg].push(n_n[0])
+                }
+            }
+            else if(n_n.length===2){
+                if(apps_f.hasOwnProperty(n_n[1])){
+                    apps_f[n_n[1]].push(n_n[0]);
+                }
+                else
+                {
+                    apps_f[n_n[1]]=[];
+                    apps_f[n_n[1]].push(n_n[0])
+                }
+            }
+            else{
+                notifys.push(`ID error:【${n}】`)
+            }
+        }
+        else{
+            notifys.push(`ID error:【${n}】`)
+        }
+    });
+    if(Object.keys(apps_f).length>0){
+        post_data(apps_f);
+    }
+}
+async function post_data(d) {
+    try{
+        let app_monitor=$prefs.valueForKey("app_monitor");
+        if(app_monitor===""||app_monitor===undefined){
+            app_monitor={}
+        }
+        else{
+            app_monitor=JSON.parse(app_monitor)
+        }
+        let infos={};
+        await Promise.all(Object.keys(d).map(async (k)=>{
+            let config={
+                url:'https://itunes.apple.com/lookup?id=' + d[k] + "&country=" + k,
+                method:"post"
+            };
+            await $task.fetch(config).then((res)=>{
+                let results=JSON.parse(res.body).results;
+                if(Array.isArray(results)&&results.length>0){
+                    results.forEach((x=>{
+                        infos[x.trackId]={
+                            n:x.trackName,
+                            v:x.version,
+                            p:x.formattedPrice
+                        };
+                        if(app_monitor.hasOwnProperty(x.trackId)){
+                            if(JSON.stringify(app_monitor[x.trackId])!==JSON.stringify(infos[x.trackId])){
+                                if(x.version!==app_monitor[x.trackId].v){
+                                    notifys.push(`${flag(k)}🧩${x.trackName}:version【${x.version}】`)
+                                }
+                                if(x.formattedPrice!==app_monitor[x.trackId].p){
+                                    notifys.push(`${flag(k)}💰${x.trackName}:price【${x.formattedPrice}】`)
+                                }
+                            }}
+                        else{
+                            notifys.push(`${flag(k)}🧩${x.trackName}:version【${x.version}】`);
+                            notifys.push(`${flag(k)}💰${x.trackName}:price【${x.formattedPrice}】`)
+                        }
+                    }));
+                }
+                return Promise.resolve()
+            }).catch((e)=>{
+                console.log(e);
+            });
+        }));
+        infos=JSON.stringify(infos);
+        $prefs.setValueForKey(infos,"app_monitor");
+        if(notifys.length>0){
+            notify(notifys)
+        }
+        else{
+            console.log("APPPRICE：No change 🔕")
+        }
+    }catch (e) {
+        console.log(e);
+    }
+}
+function notify(notifys){
+    notifys=notifys.join("\n");
+    console.log(notifys);
+    $notify("APPPRICE","",notifys)
+}
+function flag(x){
+  var flags = new Map([[ "AC" , "🇦🇨" ] , [ "AF" , "🇦🇫" ] , [ "AI" , "🇦🇮" ] , [ "AL" , "🇦🇱" ] , [ "AM" , "🇦🇲" ] , [ "AQ" , "🇦🇶" ] , [ "AR" , "🇦🇷" ] , [ "AS" , "🇦🇸" ] , [ "AT" , "🇦🇹" ] , [ "AU" , "🇦🇺" ] , [ "AW" , "🇦🇼" ] , [ "AX" , "🇦🇽" ] , [ "AZ" , "🇦🇿" ] , [ "BB" , "🇧🇧" ] , [ "BD" , "🇧🇩" ] , [ "BE" , "🇧🇪" ] , [ "BF" , "🇧🇫" ] , [ "BG" , "🇧🇬" ] , [ "BH" , "🇧🇭" ] , [ "BI" , "🇧🇮" ] , [ "BJ" , "🇧🇯" ] , [ "BM" , "🇧🇲" ] , [ "BN" , "🇧🇳" ] , [ "BO" , "🇧🇴" ] , [ "BR" , "🇧🇷" ] , [ "BS" , "🇧🇸" ] , [ "BT" , "🇧🇹" ] , [ "BV" , "🇧🇻" ] , [ "BW" , "🇧🇼" ] , [ "BY" , "🇧🇾" ] , [ "BZ" , "🇧🇿" ] , [ "CA" , "🇨🇦" ] , [ "CF" , "🇨🇫" ] , [ "CH" , "🇨🇭" ] , [ "CK" , "🇨🇰" ] , [ "CL" , "🇨🇱" ] , [ "CM" , "🇨🇲" ] , [ "CN" , "🇨🇳" ] , [ "CO" , "🇨🇴" ] , [ "CP" , "🇨🇵" ] , [ "CR" , "🇨🇷" ] , [ "CU" , "🇨🇺" ] , [ "CV" , "🇨🇻" ] , [ "CW" , "🇨🇼" ] , [ "CX" , "🇨🇽" ] , [ "CY" , "🇨🇾" ] , [ "CZ" , "🇨🇿" ] , [ "DE" , "🇩🇪" ] , [ "DG" , "🇩🇬" ] , [ "DJ" , "🇩🇯" ] , [ "DK" , "🇩🇰" ] , [ "DM" , "🇩🇲" ] , [ "DO" , "🇩🇴" ] , [ "DZ" , "🇩🇿" ] , [ "EA" , "🇪🇦" ] , [ "EC" , "🇪🇨" ] , [ "EE" , "🇪🇪" ] , [ "EG" , "🇪🇬" ] , [ "EH" , "🇪🇭" ] , [ "ER" , "🇪🇷" ] , [ "ES" , "🇪🇸" ] , [ "ET" , "🇪🇹" ] , [ "EU" , "🇪🇺" ] , [ "FI" , "🇫🇮" ] , [ "FJ" , "🇫🇯" ] , [ "FK" , "🇫🇰" ] , [ "FM" , "🇫🇲" ] , [ "FO" , "🇫🇴" ] , [ "FR" , "🇫🇷" ] , [ "GA" , "🇬🇦" ] , [ "GB" , "🇬🇧" ] , [ "HK" , "🇭🇰" ] , [ "ID" , "🇮🇩" ] , [ "IE" , "🇮🇪" ] , [ "IL" , "🇮🇱" ] , [ "IM" , "🇮🇲" ] , [ "IN" , "🇮🇳" ] , [ "IS" , "🇮🇸" ] , [ "IT" , "🇮🇹" ] , [ "JP" , "🇯🇵" ] , [ "KR" , "🇰🇷" ] , [ "MO" , "🇲🇴" ] , [ "MX" , "🇲🇽" ] , [ "MY" , "🇲🇾" ] , [ "NL" , "🇳🇱" ] , [ "PH" , "🇵🇭" ] , [ "RO" , "🇷🇴" ] , [ "RS" , "🇷🇸" ] , [ "RU" , "🇷🇺" ] , [ "RW" , "🇷🇼" ] , [ "SA" , "🇸🇦" ] , [ "SB" , "🇸🇧" ] , [ "SC" , "🇸🇨" ] , [ "SD" , "🇸🇩" ] , [ "SE" , "🇸🇪" ] , [ "SG" , "🇸🇬" ] , [ "TH" , "🇹🇭" ] , [ "TN" , "🇹🇳" ] , [ "TO" , "🇹🇴" ] , [ "TR" , "🇹🇷" ] , [ "TV" , "🇹🇻" ] , [ "TW" , "🇨🇳" ] , [ "UK" , "🇬🇧" ] , [ "UM" , "🇺🇲" ] , [ "US" , "🇺🇸" ] , [ "UY" , "🇺🇾" ] , [ "UZ" , "🇺🇿" ] , [ "VA" , "🇻🇦" ] , [ "VE" , "🇻🇪" ] , [ "VG" , "🇻🇬" ] , [ "VI" , "🇻🇮" ] , [ "VN" , "🇻🇳" ]])
+  return flags.get(x.toUpperCase())
+}
