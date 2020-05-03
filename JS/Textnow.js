b91cde64b66355e8e56c9689624ec16ea2d45fb3
Lê Văn Tính <lvtinh1051989@users.noreply.github.com>
2020-05-03 23:48:42 +0700

    Create

--- JS/Textnow.js
+++
@@ -1,6 +0,0 @@
-/*
-^https:\/\/api\.textnow\.me\/api2.0\/users\/.* url script-response-body Surge-JS/Textnow.js
-*/var obj = JSON.parse($response.body); 
-obj['show_ads'] = false;
-obj['premium_calling'] = true;
-$done({body: JSON.stringify(obj)});
