b0b02f5101b8ab8c3092fa38b08e05fb972f98b0
Lê Văn Tính <59911146+lvtinh1051989@users.noreply.github.com>
2020-12-16 16:02:16 +0700

    Update DaysCountdown.js

--- Cron/DaysCountdown.js
+++ Cron/DaysCountdown.js
@@ -176,7 +176,6 @@ function dateDiff(startDate, endDate) {
 const dayarr = [ 
     [ "Happy👩‍❤️‍👨Wedding", "2018-01-18" ],
     [ "Năm nay", "2020-12-31" ],
-    [ "Lễ tình nhân❤️ ", "2008-02-14" ], 
     [ "Sinh nhật Hồ", "1991-02-16" ],
     [ "Sinh nhật Tính", "1989-05-10" ],
     [ "Sinh nhật Tâm", "1991-05-19" ],
