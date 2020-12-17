69b4d4c9e3e858975839975febece9bf41c31a3e
Lê Văn Tính <lvtinh1051989@users.noreply.github.com>
2020-12-17 15:50:31 +0700

    Update

--- Surge/AppPriceSurge.js
+++ Surge/AppPriceSurge.js
@@ -1,7 +1,115 @@
+//Smart Qx&Surge 
+let isQuantumultX = $task !== undefined;
+let isSurge = $httpClient !== undefined;
+var $task = isQuantumultX ? $task : {};
+var $httpClient = isSurge ? $httpClient : {};
+var $prefs = isQuantumultX ? $prefs : {};
+var $persistentStore = isSurge ? $persistentStore : {};
+var $notify = isQuantumultX ? $notify : {};
+var $notification = isSurge ? $notification : {};
+if (isQuantumultX) {
+    var errorInfo = {
+        error: ''
+    };
+    $httpClient = {
+        get: (url, cb) => {
+            var urlObj;
+            if (typeof (url) == 'string') {
+                urlObj = {
+                    url: url
+                }
+            } else {
+                urlObj = url;
+            }
+            $task.fetch(urlObj).then(response => {
+                cb(undefined, response, response.body)
+            }, reason => {
+                errorInfo.error = reason.error;
+                cb(errorInfo, response, '')
+            })
+        },
+        post: (url, cb) => {
+            var urlObj;
+            if (typeof (url) == 'string') {
+                urlObj = {
+                    url: url
+                }
+            } else {
+                urlObj = url;
+            }
+            url.method = 'POST';
+            $task.fetch(urlObj).then(response => {
+                cb(undefined, response, response.body)
+            }, reason => {
+                errorInfo.error = reason.error;
+                cb(errorInfo, response, '')
+            })
+        }
+    }
+}
+if (isSurge) {
+    $task = {
+        fetch: url => {
+            return new Promise((resolve, reject) => {
+                if (url.method == 'POST') {
+                    $httpClient.post(url, (error, response, data) => {
+                        response.body = data;
+                        resolve(response, {
+                            error: error
+                        });
+                    })
+                } else {
+                    $httpClient.get(url, (error, response, data) => {
+                        response.body = data;
+                        resolve(response, {
+                            error: error
+                        });
+                    })
+                }
+            })
+
+        }
+    }
+}
+if (isQuantumultX) {
+    $persistentStore = {
+        read: key => {
+            return $prefs.valueForKey(key);
+        },
+        write: (val, key) => {
+            return $prefs.setValueForKey(val, key);
+        }
+    }
+}
+if (isSurge) {
+    $prefs = {
+        valueForKey: key => {
+            return $persistentStore.read(key);
+        },
+        setValueForKey: (val, key) => {
+            return $persistentStore.write(val, key);
+        }
+    }
+}
+if (isQuantumultX) {
+    $notification = {
+        post: (title, subTitle, detail) => {
+            $notify(title, subTitle, detail);
+        }
+    }
+}
+if (isSurge) {
+    $notify = function (title, subTitle, detail) {
+        $notification.post(title, subTitle, detail);
+    }
+}
+//end
+
 /*using surge cron*/
 
 const region = "vn"
-const appIds = ["596684220","1444671526","1300697619","284666222","1457771281","442012681","804177739","558818638","1373567447","932747118","1252015438","556666026","1117998129","382011064","669835030","688831692","317107309","360593530","1319618742","469343097","728293409","527358348","1389752090","479516143","1062022008","924695435","1112850631","471784765","1110639444","691121579","1116905928","1444383602","436577167","606310581","992357547","579523206","284882215","585253443","544007664","414706506","570435154","705211891","454638411","585027354","959841449","790034666","962194608","469337564","382617920","310633997","389801252","477537958","507874739","834742893","1210763463","1192965614","1001507516","333903271","864653381","1126386264","1047223162","304878510","1160167263","1061867449","324684580","393149734","1359763765","826543747","904209370","931883425","1286983622","1235601864","686449807","842842640","842849113","1034726283","1442620678","879478102","936971630","958100553","1292563772","1450468835","777554515","1439523058","947291031","1032301823","1344700109","306310789","1462680353","440676901","981863450","1462416637","751096907","1385287093","367003839","1400740705","439438619","878783582","971995033","333710667","448278467","570060128","585332633","985746746","389793068","336978041","1002043426","1223932558","388627783","1477505685","896694807","930331514","1443988620","1338092370","1422605014","541164041","1129435228","888530356","1187265767","546505307","438596432","492648096","441697527"]
+const appIds = ["1312014438","990591885","1141312799","1073473333","432850144","896694807","1434207799","924695435","680469088","869346854","935754064","1035331258","904237743","946930094","1373567447","916366645","1382419586","1299735217","1460078746","333710667","1049254261","1489780246","1407367202","436577167","1481018071","1315744137","1436650069","980368562","1007355333","1126386264","492648096","950519698","317107309","539397400","1444671526","1416894836","1117998129","1462386180","558818638","691121579","1474856599","436577167","641613694","1312014438","1416894836","1117998129","1462386180","558818638","691121579","1474856599","436577167","641613694","1312014438"]
+
 var cacheData = $persistentStore.read()
 if (!cacheData) {
     cacheData = {}
