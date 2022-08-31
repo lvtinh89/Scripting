05724a50551012e7186f13c558edf7fc5dce5ee9
Lê Văn Tính <59911146+lvtinh1051989@users.noreply.github.com>
2022-09-01 00:46:36 +0700

    Update zingmp3old.js

--- zingmp3old.js
+++ zingmp3old.js
@@ -1,14 +1,15 @@
 let obj= JSON.parse($response.body);
 
-//fake user vip
+if($response.body.indexOf("lastVipExpireTime") !=-1){
 delete obj.data.lastVipExpireTime;
+}
 obj.data.vip= {
-  "expireTime": 1892800999,
-  "startTime": 1572527803,
+  "expireTime": 4096412384000,
+  "startTime": 1572527803000,
   "subscription": {
     "status": 0,
-    "expireTime": 1892800999,
-    "platform": 2
+    "expireTime": 4096412384000,
+    "platform": 5
   },
   "vipType": 1
 }
