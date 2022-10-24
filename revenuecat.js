41f137625311a7e8a3089fc76888c0cee46f42c0
Lê Văn Tính <59911146+lvtinh1051989@users.noreply.github.com>
2022-10-24 20:11:23 +0700

    Update revenuecat.js

--- revenuecat.js
+++ revenuecat.js
@@ -1,21 +1,17 @@
-let obj = JSON.parse($response.body);
-obj["subscriber"]["subscriptions"]= {
-      "revenuecat.pro.yearly": {
-        "is_sandbox": false,
-        "period_type": "active",
-        "billing_issues_detected_at": null,
-        "unsubscribe_detected_at": null,
-        "expires_date": "2099-12-01T03:51:32Z",
-        "original_purchase_date": "2019-10-31T02:51:33Z",
-        "purchase_date": "2019-10-31T02:51:32Z",
-        "store": "app_store"
-      }
-    };
-obj["subscriber"]["entitlements"]= {
-      "pro": {
-        "expires_date": "2099-12-01T03:51:32Z",
-        "product_identifier": "revenuecat.pro.yearly",
-        "purchase_date": "2019-10-31T02:51:32Z"
-      }
-    };
-$done({body: JSON.stringify(obj)});
+re('"expires_date":"\\w{4}@"period_type":"\\w+"','"expires_date":"2099@"period_type":"active"');
+function re() {
+var body = $response.body;
+ if (arguments[0].includes("@")) {
+  var regs = arguments[0].split("@");
+  var strs = arguments[1].split("@");
+  for (i = 0;i < regs.length;i++) {
+   var reg = new RegExp(regs[i],"g");
+   body = body.replace(reg, strs[i]);
+ }
+}
+ else {
+  var reg = new RegExp(arguments[0],"g");
+  body = body.replace(reg, arguments[1]);
+}
+ $done({body});
+} 
