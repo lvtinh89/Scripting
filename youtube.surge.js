4b878ed9f981e83376098017de004153225f7929
Lê Văn Tính <59911146+lvtinh1051989@users.noreply.github.com>
2022-10-24 22:17:30 +0700

    Update youtube.surge.js

--- youtube.surge.js
+++ youtube.surge.js
@@ -1,8 +1,7 @@
-if ($request.url.indexOf("&oad") != -1) {
-  $done({ response: {body: ""} });
-} else if ($request.url.indexOf("&ctier") != -1) {
-  let url = $request.url.replace(/ctier=[A-Z]/, "ctier=A");
-  $done({ response: { status: 302, headers: { Location: url } } });
-} else {
-  $done({})
-}
+var data = {
+  body: "{}",
+  headers: {
+    "Content-Type": "multipart/byteranges"
+  }
+};
+$done({response: data});
