21573864cd5d99a5f2d9de005a416b7740e8b7a9
Lê Văn Tính <59911146+lvtinh1051989@users.noreply.github.com>
2022-11-23 08:28:38 +0700

    Update XMind.js

--- XMind.js
+++ XMind.js
@@ -1,12 +1,20 @@
-var obj = JSON.parse($response.body);
+/*
+Quantumult X:
+[rewrite_local]
+https?:\/\/.*\.xmind\..*\/_res\/devices url script-response-body XMind.js
+[mitm]
+hostname = *.xmind.*,
+*/
+
+let obj = JSON.parse($response.body);
 
 obj = {
-   "raw_data": "DBcBHgojrvPgruIJMfb4KK/76CmjxSHSp9KipRwOnQUuf+Gt2FncFUzNvxZydUeXEzDZt/XWEm91lHFYrvT0f2AFap2L4psLI/34sKU9VLGH7kFsBYlexU/nifBtosMJqQxL4TU1pjvjl+1uyWsFAeGR42aEnVhQWgvJB5Ovcd0=",
-   "license": {
-     "status": "sub",
-     "expireTime": 3070928235000
-   },
-   "_code": 200
- }
-
+  "raw_data": "S0MY6Wu5wpkW52RE5XmMkSMfTBvnytTwIJODrtVDjnA0axrORbnv9gh1RC4W3/ejTfQhNBb7CVxxpbYnBBk2tHc4gAODhsuGpHkltYNL/P5dfORSpdbiNkAZr5aBBbHS/dNlaYjLYyBkq9Ohfe0QS9PeXOWLbDdNA6kqidLJysw=",
+  "license": {
+    "status": "sub",
+    "expireTime": 9999999999999
+  },
+  "_code": 200
+}
+  
 $done({body: JSON.stringify(obj)});
