ddc2fa316323d92dbe75c99f824106df17d69f2a
Lê Văn Tính <59911146+lvtinh89@users.noreply.github.com>
2022-12-16 13:09:47 +0700

    Update PDF Expert.js

--- Surge/modules/PDF Expert.js
+++ Surge/modules/PDF Expert.js
@@ -3,4 +3,4 @@
 [MITM]
 hostname = %APPEND% license.pdfexpert.com
 [Script]
-Documents-PdfExpert = type=http-response,pattern=^https:\/\/license\.pdfexpert\.com\/api\/.+\/subscription\/(refresh$|check$),requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/lvtinh1051989/Scripting/master/pdfexpert.js,script-update-interval=-1
+Documents-PdfExpert = type=http-response,pattern=^https:\/\/license\.pdfexpert\.com\/api\/.+\/subscription\/(refresh$|check$),requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/lvtinh89/Scripting/master/Pdfexpert.js,script-update-interval=-1
