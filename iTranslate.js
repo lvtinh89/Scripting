e9415093dba7b05f2205a3256bf8a4d0a3d951c6
Lê Văn Tính <59911146+lvtinh89@users.noreply.github.com>
2023-01-31 13:56:20 +0700

    Update

--- iTranslate.js
+++ iTranslate.js
@@ -1,7 +1,13 @@
-var obj = JSON.parse($response.body);
-
-obj= {"licenses": [{"is_trial_period": true, "product_id": "com.itranslate.pro.yearly", "original_transaction_id": "20000627379146", "expires_date_ms": 4099246456000, "bundle_id": "com.outerspaceapps.itranslate", "transaction_id": "20000627379146"}, {"is_trial_period": true, "product_id": "com.itranslate.pro.yearly", "original_transaction_id": "20000627379146", "expires_date_ms": 4099246456000, "bundle_id": "com.outerspaceapps.itranslate", "transaction_id": "20000627379146"}]};
-
+let obj= {
+  "licenses": [
+    {
+      "product_id": "com.itranslate.pro.yearly",
+      "is_trial_period": false,
+      "original_transaction_id": "440001324215869",
+      "transaction_id": "440001324215869",
+      "expires_date_ms": 4099246456000,
+      "bundle_id": "com.outerspaceapps.itranslate"
+    }
+  ]
+}
 $done({body: JSON.stringify(obj)});
-
-// Descriptions
