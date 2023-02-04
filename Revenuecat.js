11902d9f1225a7a43c5702cf15a718ccb211b50f
Lê Văn Tính <59911146+lvtinh89@users.noreply.github.com>
2023-02-04 22:20:49 +0700

    Update Revenuecat.js

--- Revenuecat.js
+++ Revenuecat.js
@@ -8,6 +8,7 @@ const list = {
 	'Anybox': { name: 'pro', id: 'cc.anybox.Anybox.annual' },
 	'Fileball': { name: 'filebox_pro', id: 'com.premium.yearly' },
 	'ipTV': { name: 'ipTV +', id: 'iptv_9.99_1y_7d_free' },
+	'APTV': { name: 'pro', id: 'com.kimen.aptvpro.lifetime' },
 	'Speechify': { name: 'pro', id: 'com.cliffweitzman.speechifyMobile2.premium.annual' },
 	'Blink': { name: 'pro', id: 'blink_shell_plus_1y_1999' },
         'mizframa': { name: 'premium', id: 'mf_20_lifetime2' },
