8cc09269ff9a73791160dac704466564de1143e2
Lê Văn Tính <59911146+lvtinh89@users.noreply.github.com>
2023-02-06 22:21:59 +0700

    Update

---
+++ Kidsup.js
@@ -0,0 +1,13 @@
+var obj = JSON.parse($response.body);
+
+obj= {
+ "response": {
+  "id": "full",
+  "title": "TRỌN ĐỜI",
+  "orderId": 87543,
+  "unlockGift": false,
+  "prepaid": false
+ }
+};
+
+$done({body: JSON.stringify(obj), status: 200});
