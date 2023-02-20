e0df51667a12bc5ae83dfbf60742a45e93bae32c
Lê Văn Tính <59911146+lvtinh89@users.noreply.github.com>
2023-02-20 13:38:11 +0700

    Update

--- Surge/Panels/Network-Info.js
+++ Surge/Panels/Network-Info.js
@@ -176,9 +176,9 @@ function getIP() {
     info = ['internet bị mất mạng', 'Vui lòng làm mới thủ công để kết nối lại IP'];
   } else {
     if (v4?.primaryAddress) info.push(`Thiết bị IP：${v4?.primaryAddress}`);
-    if (v6?.primaryAddress) info.push(`Địa chỉ IPv6：Được phân bổ`);
+    if (v6?.primaryAddress) info.push(`Địa chỉ IPv6：Được bảo mật`);
     if (v4?.primaryRouter && getSSID()) info.push(`Bộ định tuyến IP：${v4?.primaryRouter}`);
-    if (v6?.primaryRouter && getSSID()) info.push(`Địa chỉ IPv6：Được phân bổ`);
+    if (v6?.primaryRouter && getSSID()) info.push(`Địa chỉ IPv6：Được bảo mật`);
   }
   info = info.join("\n");
   return info + "\n";
