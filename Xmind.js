dc58a23e4fba2f48b31e63699ba7f1fb55ffd532
Lê Văn Tính <59911146+lvtinh89@users.noreply.github.com>
2023-10-29 21:51:27 +0700

    Update Xmind.js

--- Xmind.js
+++ Xmind.js
@@ -1,6 +1,12 @@
-let obj = JSON.parse($response.body);
-obj.license = {
-    "status": "sub",
-    "expireTime": 4099363624000
-  }
-$done({body: JSON.stringify(obj)});
+var baby = JSON.parse($response.body);
+
+baby = {
+  "license" : {
+    "status" : "sub",
+    "expireTime" : 101966821994000
+  },
+  "_code" : 200,
+  "raw_data" : "bKnTeMMu/TJ86ZFoUqG+sfKC1nitD8b/NaUT93UBc0woo0BF5uBVYXsHUQJkQxLHGlZCbdnKaKzZ4iljM9xddkyMycpUbAtDoywnr4RNzxqXY20pDM0Bdgm+HssaS1RworH8aaaf0IYnnGplxIuqMyT9107PrenF0MBUIaKU9Cw="
+};
+
+$done({body : JSON.stringify(baby)});
