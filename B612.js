474e8dd9875df33131c8ab2dcceaf976e5dcc729
Lê Văn Tính <59911146+lvtinh89@users.noreply.github.com>
2023-11-10 10:36:36 +0700

    Create B612.js

---
+++ B612.js
@@ -0,0 +1,15 @@
+let obj= {
+  "result": {
+    "products": [
+      {
+        "managed": false,
+        "status": "ACTIVE",
+        "startDate": 1676119763000,
+        "productId": "com.linecorp.b612.vip.oneyear",
+        "expireDate": 4073547412000
+      }
+    ],
+    "activated": true
+  }
+	};
+$done({body: JSON.stringify(obj)})
